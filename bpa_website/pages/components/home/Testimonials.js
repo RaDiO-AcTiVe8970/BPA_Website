466d7c9efdc306704ebb8d18bc7cc259d820e8b1
RaDiO-AcTiVe8970 <62706964+RaDiO-AcTiVe8970@users.noreply.github.com>
2023-10-05 12:56:52 +0600

    initAdmin

--- bpa_website/pages/components/home/Testimonials.js
+++ bpa_website/pages/components/home/Testimonials.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+/*import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { Element, scroller } from 'react-scroll';
 
@@ -100,6 +100,95 @@ function Testimonials() {
                   </figure>
                   <div className="card-body items-center text-center">
                     <p className="text-black">{testimonial.text}</p>
+                    <p className="text-slate-500">-{testimonial.name}</p>
+                  </div>
+                </motion.div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Element>
+  );
+}
+
+export default Testimonials;*/
+
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { Element, scroller } from 'react-scroll';
+
+function Testimonials() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [testimonials, setTestimonials] = useState([]);
+
+  useEffect(() => {
+    // Listen for scroll events to trigger animations
+    window.addEventListener('scroll', handleScroll);
+    fetchData();
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    // Calculate the position of the Testimonials section
+    const testimonialsSection = document.getElementById('testimonialsDiv');
+    if (testimonialsSection) {
+      const testimonialsSectionTop = testimonialsSection.getBoundingClientRect().top;
+
+      // Check if the Testimonials section is in the viewport
+      if (testimonialsSectionTop < window.innerHeight * 0.75) {
+        setIsVisible(true);
+      }
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      // Replace this with your actual API URL
+      const response = await fetch('http://localhost:3000/api/bpa/admin/getAllTestimonials');
+      const data = await response.json();
+
+      // Set the testimonials data in the state
+      setTestimonials(data);
+    } catch (error) {
+      console.error('Error fetching testimonials:', error);
+    }
+  };
+
+  return (
+    <Element name="testimonialsSection">
+      <div
+        id="testimonialsDiv"
+        className="grid place-items-center w-full bg-base-100"
+        data-theme="cupcake"
+      >
+        <div className="max-w-5xl py-24 content-center justify-center">
+          <h1 className="text-4xl text-center text-black font-bold">Testimonials</h1>
+          <div className="grid mt-12 md:grid-cols-3 grid-cols-1 gap-8">
+            {testimonials.map((testimonial, key) => {
+              return (
+                <motion.div
+                  key={key}
+                  className={`card w-full bg-base-100 shadow-xl ${
+                    isVisible ? 'animate-fade-in' : 'opacity-0'
+                  }`}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
+                  transition={{ duration: 0.5, delay: key * 0.3 }}
+                  whileHover={{ scale: 1.05 }}
+                >
+                  <figure className="px-10 pt-10">
+                    {/* Use the actual image path */}
+                    <img
+                      className="mask w-16 h-16 mask-circle"
+                      src={testimonial.image} // Replace with the image path
+                      alt=""
+                    />
+                  </figure>
+                  <div className="card-body items-center text-center">
+                    <p className="text-black">{testimonial.description}</p>
                     <p className="text-slate-500">-{testimonial.name}</p>
                   </div>
                 </motion.div>
